70fbb3ae7ea7b8d31ed3bf3b7ed2c86f1ec23faa
Korawich Anuttra <korawich.anu@gmail.com>
2025-07-11 21:51:28 +0700

    :test_tube: tests: update model testcase.

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,12 +4,12 @@ from typing import ClassVar
 from deflow.assets.models import AbstractModel
 
 
-class TestAbstractModel(AbstractModel):
+class MockAbstractModel(AbstractModel):
     check_type: ClassVar[str] = "Pipeline"
 
 
 def test_abs_model(test_path: Path):
-    data = TestAbstractModel.from_conf(
+    data = MockAbstractModel.from_conf(
         "p_pipe_cm_d", path=test_path / "v2/conf"
     )
     assert data.type == "Pipeline"
